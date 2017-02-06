92e598c0b258d63b03dab81153f9d7755602076b
betolp100 <beto11_lp@hotmail.com>
2017-02-05 18:31:47 -0600

    Update FirstPartialExercises.java

--- FirstPartialExercises/src/firstpartialexercises/FirstPartialExercises.java
+++ FirstPartialExercises/src/firstpartialexercises/FirstPartialExercises.java
@@ -3,13 +3,13 @@ package firstpartialexercises;
 
 public class FirstPartialExercises {
 public static void main(String[] args) {
-        MyRectangle test = new MyRectangle(2,0,0,2);
-        System.out.println(test);
-        System.out.println(test.getArea());
+        MyRectangle testDriver = new MyRectangle(2,0,0,2);
+        System.out.println(testDriver);
+        System.out.println(testDriver.getArea());
         MyPoint a = new MyPoint(3,0);
         MyPoint b = new MyPoint(0,3);
-        MyRectangle test2 = new MyRectangle(a,b);
-        System.out.println(test2);
-        System.out.println(test2.getArea());
+        MyRectangle testDriver2 = new MyRectangle(a,b);
+        System.out.println(testDriver2);
+        System.out.println(testDriver2.getArea());
     }
 }
